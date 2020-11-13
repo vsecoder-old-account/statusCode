fae769e416305a1de0bf1693609dc742077a6096
Всеволод html <63463168+htmlcssphpjs@users.noreply.github.com>
2020-11-13 20:05:13 +0300

    #2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var url = require('url');
 const request = require('request');
+
 var app = express();
 getClientAddress = function (req) {
 	return (req.headers['x-forwarded-for'] || '').split(',')[0] 
@@ -29,9 +30,9 @@ app.get('/', function(req, res) {
 			//console.log('body:', body);
 			var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
 			if (error == null) {
-				return res.render('home', { error: error, statusCode: response.statusCode, ip: ip, body: body });
+				return res.render('home', { statusCode: response.statusCode, ip: ip, body: body });
 			} else {
-				return res.render('home', { error: error, statusCode: error, ip: ip, body: body });
+				return res.render('home', { statusCode: error, ip: ip, body: body });
 			}
 		});
 	} else {
@@ -55,6 +56,42 @@ app.get('/api', function(req, res) {
 	} else {
 		return res.json('!!!no url!!!');
 	}
+});
+app.get('/code', function(req, res) {
+	var href = req.query.url;
+	if (href) {
+		request(href, function (error, response, body) {
+			console.error('error:', error);
+			console.log('statusCode:', response && response.statusCode);
+			//console.log('body:', body);
+			var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
+			if (error == null) {
+				return res.json(body);
+			} else {
+				return res.json(body);
+			}
+		});
+	} else {
+		var ip = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress;
+		return res.json('/?url=NULL');
+	}
+});
+
+app.use("/404",function (request, response) {
+	response.status(404);
+	response.json(`404 not found`);
+});
+app.use("/500",function (request, response) {
+	response.status(404);
+	response.json(`500 server error`);
+});
+app.use("/429",function (request, response) {
+	response.status(404);
+	response.json(`429 to many requests`);
+});
+app.use("/:stcode",function (request, response) {
+	response.status(request.params.stcode);
+	response.json(request.params.stcode + ' error');
 });
 
 // 404 catch-all handler (middleware)
